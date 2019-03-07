ffcd53606ba58bd048f7eb1c2537481f7a238981
anil.devdutt <anil.devdutt@gmail.com>
2019-03-07 00:31:22 -0500

    Home Feed

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
   _ "github.com/go-sql-driver/mysql"
 )
 
+type Homefeed struct {
+  Headlines string
+  Content string
+}
+
 var t *template.Template
 func init() {
   t = template.Must(template.ParseGlob("html/*.html"))
@@ -29,9 +34,21 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
   db, err := sql.Open("mysql", "root:password@/aerothon")
   defer db.Close()
   
-  results, err := db.Query("SELECT id, name FROM tags")
+  results, err := db.Query("SELECT headlines, content FROM homefeed")
   if err != nil {
     log.Fatalln(err)
   }
-  t.ExecuteTemplate(w, "home.html", nil)
+  var homeFeeds []Homefeed
+  for results.Next() {
+		var homeFeed Homefeed
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&homeFeed.Headlines, &homeFeed.Content)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+ 
+    homeFeeds = append(homeFeeds, homeFeed)
+	}
+  fmt.Println(homeFeeds)
+  t.ExecuteTemplate(w, "home.html", homeFeeds)
 }
